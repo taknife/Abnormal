5e9e0176cfdd428467b19bf48cfc00a2729e4c50
Kuns <2524374137@qq.com>
2022-09-26 15:26:00 +0800

    新增jolt2模块

---
+++ venv/src/jolt2.py
@@ -0,0 +1,22 @@
+# #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project -> @File : Abnormal -> jolt2.py
+# @Author   : Taknife
+# @IDE      : PyCharm
+# @Time     : 2022/9/26 15:20
+
+"""
+-------------------- Function --------------------
+Desc: 
+---------------------- End -----------------------
+"""
+
+from scapy.all import *
+
+
+def crafting_packets():
+    pass
+
+
+if __name__ == '__main__':
+    pass
